616001ddcf3e034d9f95e3572f708e5a69565954
DmitriyYakovenko <dmitriyyakov@gmail.com>
2017-02-06 17:26:33 +0200

    f commit

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://dev1.dreamvention.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://dev1.dreamvention.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home/dev1/public_html/catalog/');
+define('DIR_SYSTEM', '/home/dev1/public_html/system/');
+define('DIR_LANGUAGE', '/home/dev1/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/home/dev1/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/home/dev1/public_html/system/config/');
+define('DIR_IMAGE', '/home/dev1/public_html/image/');
+define('DIR_CACHE', '/home/dev1/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/dev1/public_html/system/storage/download/');
+define('DIR_LOGS', '/home/dev1/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/dev1/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home/dev1/public_html/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'user');
+define('DB_PASSWORD', 'user1234');
+define('DB_DATABASE', 'developer_1');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
